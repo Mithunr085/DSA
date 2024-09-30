aff6ca804f7ce8f2b12ef06c3312b2e370330c0c
Mithunr085 <mithunrmithunr2@gmail.com>
2024-09-30 21:43:03 +0530

    Create repeatedElements.cpp

---
+++ Arrays(Vectors).cpp/repeatedElements.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void repeatedElem(int a[], int n) // to print
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (i != j && a[i] == a[j])
+            {
+                cout << a[i] << " ";
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Number of Elements in Array : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the Elements of Array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    repeatedElem(arr, n);
+    return 0;
+}
